c8f28c74db858fca9ed3a2a1167d441af47df218
kenneththomas <kennethwoo11@gmail.com>
2020-03-24 11:56:04 -0400

    ARI-3: Total Row for Daily Stats, By Hour

--- scripts/genstats.py
+++ scripts/genstats.py
@@ -41,6 +41,13 @@ print(dates)
 
 for date in datelist:
     dexport = ['User,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,Total']
+
+    # this ordered dictionary will contain the message total for all users during this day
+    dailytotal = odict({})
+    # populate dailytotal with each hour
+    for i in range (0,24):
+        dailytotal[str(i)] = 0
+
     filename = '../stats/daily/{}.csv'.format(date)
     # create dataframe only matching this particular day
     df2 = (df['Timestamp'].dt.date == date)
@@ -70,13 +77,14 @@ for date in datelist:
             a = hour[0] # which hour
             b = hour[-1] # how many messages during this hour
             uhdict[str(a)] = str(b)
+            dailytotal[str(a)] = dailytotal[str(a)] + int(b)
 
         newstring = user
         for i in uhdict.keys():
             x = uhdict[str(i)]
             newstring = newstring + ',' + x
 
-            # to get a total for the day
+            # to get a user's total for the day
             usermessagecount += int(x)
 
 
@@ -85,19 +93,30 @@ for date in datelist:
 
         dexport.append(newstring)
 
+    # total row for day
+    dailymessagecount = 0
+    dailystring = 'TOTAL'
+    for i in dailytotal.keys():
+        x = dailytotal[str(i)]
+        dailystring = dailystring + ',' + str(x)
+
+        # to get a total for the day
+        dailymessagecount += int(x)
+
+    dailystring = dailystring + ',' + str(dailymessagecount)
+
 
     # export
     dayfile = open(filename,'w')
     for line in dexport:
         dayfile.write(line + '\n')
+    dayfile.write(dailystring + '\n')
     dayfile.close()
 
 
 # count by user totals
 cbu = df['User'].value_counts(dropna=True)
 print(cbu)
-
-#print(df.Timestamp.dt.date.head())
 
 # export count by user
 cbu.to_csv('../stats/totals.csv')
