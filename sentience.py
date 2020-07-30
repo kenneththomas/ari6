c418a2b425dc47d43386968f17e647730c0793c6
kenneththomas <kennethwoo11@gmail.com>
2020-07-30 12:33:52 -0400

    KEN-167 cleanup battlerap KEN-168 repeat messages after 4x

--- sentience.py
+++ sentience.py
@@ -1,9 +1,12 @@
 import markovify
+import mememgr as mm
 
 with open('logs/everyone.log') as jtxt:
     text = jtxt.read()
 
-jesus_text_model = markovify.NewlineText(text)
+text_model = markovify.NewlineText(text)
 
 def genmsg():
-    return jesus_text_model.make_short_sentence(140)
+    basemessage = text_model.make_short_sentence(140)
+    finalmessage = mm.battlerap_cleanup(basemessage)
+    return finalmessage
