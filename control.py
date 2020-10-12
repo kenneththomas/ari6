3c200cf2c4fc359f2ae440550443b0ff12bef9fb
kenneththomas <kennethwoo11@gmail.com>
2020-10-12 12:31:50 -0400

    KEN-227 prototype of release banned words, add admins by message

--- control.py
+++ control.py
@@ -1,6 +1,7 @@
 #config
 adminlist=['breezyexcursion#9570']
 bannedwords = ['netorare','periodt']
+heldmsgcontainer = []
 
 def admincheck(user):
     if user in adminlist:
@@ -10,13 +11,14 @@ def admincheck(user):
         print('Control: {} not authorized to run admin command'.format(user))
         return False
 
-def bannedwordsmgr(message, author):
+def controlmgr(message, author):
     class bwmpayload():
         delete = False
         message = False
+    class heldmessage():
+        author = False
+        message = False
     bwm = bwmpayload()
-
-
 
     if message == '!bw list':
         print('BWM: received list request')
@@ -31,16 +33,37 @@ def bannedwordsmgr(message, author):
                 bannedwords.append(addword[2])
                 print('BWM: added {} to the banned words list'.format(addword[2]))
                 bwm.message = 'Added {} to the banned words list'.format(addword[2])
-            if message.startswith('!bw remove'):
+            elif message.startswith('!bw remove'):
                 removeword = message.split(' ')
                 bannedwords.remove(removeword[2])
                 print('BWM: removed {} from the banned words list'.format(removeword[2]))
                 bwm.message = 'Removed {} from the banned words list'.format(removeword[2])
+            elif message.startswith('!admin add'):
+                adduser = message.split(' ')
+                adminlist.append(adduser[2])
+                bwm.message = 'Added {} to admin list'.format(adduser[2])
+            elif message.startswith('!admin remove'):
+                removeuser = message.split(' ')
+                adminlist.remove(removeuser[2])
+                bwm.message = 'Removed {} from admin list.'.format(adduser[2])
+            elif message == '!release':
+                released = ''
+                for x in heldmsgcontainer:
+                    released = released + '{}: {}'.format(x.author,x.message)
+                    if not bwm.message:
+                        bwm.message = released + '\n'
+                    else:
+                        bwm.message = bwm.message + released + '\n'
+                    heldmsgcontainer.remove(x)
             return bwm
 
     for bannedword in bannedwords:
         if bannedword in message:
             print('BWM: detected message with banned phrase {}'.format(bannedword))
+            held = heldmessage()
+            held.author = author
+            held.message = message
+            heldmsgcontainer.append(held)
             bwm.delete = True
 
     return bwm
