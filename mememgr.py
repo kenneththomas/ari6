f2c1917a928e4b6be6bdab6f1ebb25867e8e1e03
kenneththomas <kennethwoo11@gmail.com>
2022-10-06 19:19:46 -0400

    load memes from file

--- mememgr.py
+++ mememgr.py
@@ -35,36 +35,42 @@ def repeater(message):
         else:
             return False
 
+def meme_loader():
 
+    print('initializing meme loader')
+    #memebox will be populated with the content of resources/memes.csv and returned for mememgr to load memes
+    global memebox
+    memebox = {}
+    memefile = open('resources/memes.csv')
+    memedata = memefile.readlines()
+    
+    for meme in memedata:
+        
+        memesplit = meme.split(',')
+        #do not use header
+        if memesplit[0] == 'trigger':
+            continue
+        memebox[memesplit[0]] = memesplit
+
+    memefile.close()
+    
+    return memebox
+
+    
 def memes(message):
-
+    
     mememessages=[] #contains all messages that mememgr will return
 
     if repeater(message):
         mememessages.append(message)
-    
-    if message == 'hi':
-        if chance(6):
-            mememessages.append('hi')
 
-    if message == '+':
-        if chance(6):
-            mememessages.append('+')
+    #memebox functionality
+    if message in memebox.keys():
+        response = memebox[message][1].rstrip()
+        chancevalue = memebox[message][2]
 
-    if message == 'a':
-        if chance(7):
-            mememessages.append('a')
-
-    if message == 'push me to the edge':
-        mememessages.append('all my friends are dead')
-
-    if message == 'all my friends are dead':
-        mememessages.append('push me to the edge')
-
-    if 'how do you' in message:
-        if chance(4):
-            mememessages.append('very carefully!')
-
+        if chance(int(chancevalue)):
+            mememessages.append(response)
 
     if ' dn' in message:
         if message == 'whats dn' or message == 'what\'s dn':
@@ -80,18 +86,6 @@ def memes(message):
         print('MemeMgr: Found BGB Match')
         mememessages.append(response)
 
-    #rare pepes
-    if chance(99999):
-        rarepicker = messagepicker(4)
-        if rarepicker == 1:
-            mememessages.append('zip zop')
-        if rarepicker == 2:
-            mememessages.append('smells like catupto in here')
-        if rarepicker == 3:
-            mememessages.append('hey evryone whatsup gamboys')
-        if rarepicker == 4:
-            mememessages.append('whats cat up to')
-
     if message.startswith("t!weather"):
         #dont do this if a zip code, emoji, or obvious nonlocation
         if message.split()[1].startswith('<'):
@@ -100,8 +94,6 @@ def memes(message):
             int(message.split()[1])
         except ValueError:
             mememessages.append(message[10:] + " " + message[10] + 'ory')
-
-    
 
     return mememessages
 
