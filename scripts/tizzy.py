0bf5636c24743d51ab17b9751fa5c6ebbfc9c24a
Kenneth Woo <kennethwoo11@gmail.com>
2024-02-20 21:47:00 -0500

    do a word cloud by user

--- scripts/tizzy.py
+++ scripts/tizzy.py
@@ -2,26 +2,37 @@ import sqlite3
 import pandas as pd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import sys
+
+#add logic to accept a username (sender in db) as cli argument
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    username = None
 
 # Connect to your local SQLite database
 conn = sqlite3.connect('logs/gato.db')  # replace with your database file path
 cursor = conn.cursor()
 
-# Query to select all messages from the database
-cursor.execute("SELECT message FROM logs")  # replace 'your_table_name' with your table name
+#if username is provided, query for messages from that user
+if username:
+    cursor.execute("SELECT message FROM logs WHERE sender=?", (username,))
+else:
+    # Query to select all messages from the database
+    cursor.execute("SELECT message FROM logs")  # replace 'your_table_name' with your table name
 messages = cursor.fetchall()
 
 # Convert messages to a single string
 text = " ".join(message[0] for message in messages)
 
 # List of words to exclude (blacklist)
-blacklist = ['https','this','that','they','tenor','just','with','have','good','like','what','when','view']  # Add your own words here
+blacklist = ['https','this','that','they','tenor','just','with','have','good','like','what','when','view','youtube','discordapp','attachments','vxtwitter','status']  # Add your own words here
 
 # Generate the word cloud, excluding words less than 3 characters and in the blacklist
 wordcloud = WordCloud(width = 800, height = 800, 
                 background_color ='white', 
                 stopwords = blacklist,
-                min_word_length = 6).generate(text)
+                min_word_length = 5).generate(text)
 
 # Plot the WordCloud image                        
 plt.figure(figsize = (8, 8), facecolor = None) 
