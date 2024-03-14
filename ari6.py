58dbf4926abfbd08de81859245290ffcc60649c7
Kenneth Woo <kennethwoo11@gmail.com>
2024-03-13 20:00:20 -0400

    comment cleanup

--- ari6.py
+++ ari6.py
@@ -11,7 +11,7 @@ import re
 import random
 import ari_webhooks as wl
 
-ari_version = '8.4.1'
+ari_version = '8.4.2'
 
 emoji_storage = {
     'eheu': '<:eheu:233869216002998272>',
@@ -78,7 +78,6 @@ async def on_message(message):
     if len(experimental_container) > 10:
         experimental_container.pop(0)
 
-    #system
     if str(message.content).startswith('!version'):
         await message.channel.send(ari_version)
 
@@ -92,12 +91,11 @@ async def on_message(message):
         uptime_string = f"{hours} hours, {minutes} minutes, {seconds} seconds"
         await message.channel.send(uptime_string)
 
-    #if username is breezyexcursion
     if str(message.author) == 'breezyexcursion':
         if 'vxtwitter.com' not in message.content:
             if 'x.com' in message.content:
                 tweetlink = message.content.replace('x.com','vxtwitter.com')
-                await message.delete()  # delete the original message
+                await message.delete() 
                 if str(message.channel) == 'gato':
                     catchannel = client.get_channel(205903143471415296)
                     webhooks = await catchannel.webhooks()
@@ -110,7 +108,6 @@ async def on_message(message):
                 else:
                     await message.channel.send(f"{message.author.display_name} posted:\n {tweetlink}")
 
-    #toggle main_enabled with !main toggle
     if str(message.content).startswith('!main'):
         if str(message.content).replace('!main','').strip() == 'enable':
             main_enabled = True
@@ -236,9 +233,7 @@ async def on_message(message):
 
 
     if main_enabled == False:
-        #if channel id is 205903143471415296, return
         if message.channel.id == 205903143471415296:
-            # if it seems like someone is trying to run a command
             if str(message.content).startswith('!'):
                 print(f'seems like someone is trying to run a command! main disabled tho lol')
             return
@@ -269,37 +264,6 @@ async def on_message(message):
             response_text = await sentience.generate_text_with_timeout_gpt(message.content)
             await asyncio.sleep(1)
             await message.reply(response_text)
-
-
-    '''
-    #call ai_experimental from sentience
-    if mememgr.chance(3):
-        freemsg = await sentience.ai_experimental(experimental_container, 'gpt-4-1106-preview')
-
-        if freemsg:
-            experimental_container.append(f'dustin: {freemsg}')
-            catchannel = client.get_channel(205930498034237451)
-            webhooks = await catchannel.webhooks()
-            ari_webhook = next((webhook for webhook in webhooks if webhook.name == 'ari'), None)
-            if not ari_webhook:
-                ari_webhook = await catchannel.create_webhook(name='ari')
-            await ari_webhook.send(freemsg, username='ari', avatar_url='https://res.cloudinary.com/dr2rzyu6p/image/upload/v1702772600/outream/xpa09ll1hpuk3wab0jvr.png')
-
-            #call this again a random number of times from 1-3
-
-            for i in range(random.randint(1,3)):
-                personality = random.choice(list(webhook_library.values()))
-                username = personality[0]
-                avatar = personality[1]
-                system_prompt = personality[2]
-                freemsg = await sentience.ai_experimental(experimental_container, 'gpt-4-1106-preview', system_prompt)
-                experimental_container.append(f'{username}: {freemsg}')
-                await ari_webhook.send(freemsg, username=username, avatar_url=avatar)
-                await asyncio.sleep(random.randint(1,9))
-
-            return
-
-    '''
 
     global spanishmode
     if spanishmode:
@@ -371,7 +335,6 @@ async def on_message(message):
 
 
     # TODO - this block both replies and reacts so it doesnt fit in emoji reactor or memes
-    #if message content has a link in it, check if it's a twitter link
     if message.content.lower().startswith('im '):
         chrasemoji = '<:chras:237738874930069505>'
         chrasreply = message.content.lower()[2:].lstrip()
@@ -422,7 +385,6 @@ async def on_message(message):
             ari_webhook = await catchannel.create_webhook(name='ari')
     
 
-
         #append to tweetcontainer
         #if it is a duplicate, message.reply with "old"
         if message.content in tweetcontainer:
@@ -439,24 +401,6 @@ async def on_message(message):
         tootlist = aritooter.tootcontrol(toot)
         for tootmsg in tootlist:
             await message.channel.send(tootmsg)
-
-
-
-'''
-    currenttime = datetime.datetime.now()
-    # get most recent item from time_container. if it's older than 5 minutes generate a new response
-    try:
-        lasttime = time_container[-1]
-    except IndexError:
-        lasttime = datetime.datetime.now()
-    if (currenttime - lasttime).total_seconds() > 120:
-        if str(message.channel) == 'gato':
-            response_text = await sentience.generate_text_with_timeout_cm(message.content,mememgr.cleanup_username(str(message.author.name)),cm_personality)
-            await asyncio.sleep(1)
-            await message.reply(response_text)
-            bottime = datetime.datetime.now()
-            time_container.append(bottime)
-'''
 
 
 @client.event
